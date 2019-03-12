466979b0f547a9acf0964b8b429031dfa43bfdc4
Bruno Paixao <brunopaixao87@gmail.com>
2019-03-11 23:08:29 -0300

    adicionando preview add cart

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="content">
+
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src">
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left">
+          <img :src="selectedRobot.torso.src">
+          <img :src="selectedRobot.rightArm.src" class="rotate-right">
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src">
+        </div>
+      </div>
+    </div>
+
     <button class="button-add-to-cart" @click="addToCart()">Add to Cart</button>
+
     <div class="top-row">
       <!--  condicional class, :class[nomeFuncaoCriadaNoComputed] ou :style
       <div class="top part" :class="{'sale-border': selectedRobot.head.onSale}">
@@ -91,7 +109,7 @@ export default {
   methods: {
     addToCart() {
       const robot = this.selectedRobot;
-      const cost = robot.head.cost
+      const cost =        robot.head.cost
         + robot.leftArm.cost
         + robot.torso.cost
         + robot.rightArm.cost
@@ -231,5 +249,27 @@ th {
 
 .sale-border {
   border: 3px solid red;
+}
+
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
