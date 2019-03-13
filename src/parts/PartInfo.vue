9224fdf57da831135b4641a922f292bbe2d69d0f
Bruno Paixao <brunopaixao87@gmail.com>
2019-03-13 20:48:08 -0300

    parametros router com props

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -13,9 +13,19 @@ import parts from '../data/parts';
 
 export default {
   name: 'PartInfo',
+  props: {
+    partType: { type: String },
+    id: {
+      type: [Number, String],
+      validator(valeu) {
+        return Number.isInteger(Number(valeu));
+      },
+    },
+  },
   computed: {
     part() {
-      const { id, partType } = this.$route.params;
+      // parametro via route const { id, partType } = this.$route.params;
+      const { id, partType } = this;
       // + cast string para number
       return parts[partType].find(p => p.id === +id);
     },
