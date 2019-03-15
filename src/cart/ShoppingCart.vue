0e490fa47ae1797f3a5bb412c1f87c906d619ac8
Bruno Paixao <brunopaixao87@gmail.com>
2019-03-14 21:02:20 -0300

    vuex getters to return manipulation data

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -4,26 +4,33 @@
     <table>
       <thead>
         <tr>
-          <th class="robot-title">
-            Robot
-          </th>
-          <th class="cost">
-            Cost
-          </th>
+          <th class="robot-title">Robot</th>
+          <th class="cost">Cost</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(robot, index) in cart" :key="index">
-          <td class="robot-title">
-            {{robot.head.title}}
-          </td>
-          <td class="cost">
-            {{robot.cost}}
-          </td>
+          <td class="robot-title">{{robot.head.title}}</td>
+          <td class="cost">{{robot.cost}}</td>
         </tr>
       </tbody>
     </table>
- </div>
+    <h2 class="saleItems">You saved money on these robots:</h2>
+    <table>
+      <thead>
+        <tr>
+          <th class="robot-title">Robot</th>
+          <th class="cost">Cost</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(robot, index) in cartSaleItems" :key="index">
+          <td class="robot-title">{{robot.head.title}}</td>
+          <td class="cost">{{robot.cost}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -33,19 +40,28 @@ export default {
     cart() {
       return this.$store.state.cart;
     },
+    cartSaleItems() {
+      return this.$store.getters.cartSaleItems;
+    },
   },
 };
 </script>
 
 <style scoped>
-  td, th {
-    padding: 5px;
-  }
-  .robot-title {
-    text-align: left;
-    padding-right: 200px;
-  }
-  .cost {
-    text-align: right;
-  }
+td,
+th {
+  padding: 5px;
+}
+.robot-title {
+  text-align: left;
+  padding-right: 200px;
+}
+.cost {
+  text-align: right;
+}
+.saleItems {
+  margin-top: 50px;
+  font-size: 18px;
+  color: red;
+}
 </style>
