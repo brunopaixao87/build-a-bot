a6ff39ce6718a0fa6304afb98d6730c958150c21
Bruno Paixao <brunopaixao87@gmail.com>
2019-03-14 21:08:41 -0300

    adicionando chamadas http - primeiro install axios

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,11 @@ export default new Vuex.Store({
       state.cart.push(robot);
     },
   },
+  actions: {
+    getParts({ commit }) {
+
+    },
+  },
   getters: {
     cartSaleItems(state) {
       return state.cart.filter(item => item.head.onSale);
