94f524ef517a2862174c88b3c6569354fb164a43
Bruno Paixao <brunopaixao87@gmail.com>
2019-03-17 10:51:41 -0300

    criando directivas modo simplificado para lifecycle hooks bind e update

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,16 +1,7 @@
-function applyStyle(element, binding) {
+export default function (element, binding) {
   element.style.position = 'absolute';
   Object.keys(binding.value)
     .forEach((position) => {
       element.style[position] = binding.value[position];
     });
 }
-
-export default {
-  bind: (element, binding) => {
-    applyStyle(element, binding);
-  },
-  update: {
-    applyStyle(element, binding);
-  },
-};
