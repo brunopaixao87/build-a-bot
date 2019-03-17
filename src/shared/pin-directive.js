9b9c1cd3c5a62c10aa13a9e7cede48c2ec7d3340
Bruno Paixao <brunopaixao87@gmail.com>
2019-03-17 10:48:07 -0300

    diretivas update lifecycle hooks

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,9 +1,16 @@
+function applyStyle(element, binding) {
+  element.style.position = 'absolute';
+  Object.keys(binding.value)
+    .forEach((position) => {
+      element.style[position] = binding.value[position];
+    });
+}
+
 export default {
   bind: (element, binding) => {
-    element.style.position = 'absolute';
-    Object.keys(binding.value)
-      .forEach((position) => {
-        element.style[position] = binding.value[position];
-      });
+    applyStyle(element, binding);
+  },
+  update: {
+    applyStyle(element, binding);
   },
 };
